5baf4e9164f2e91baad0e35a6c4761e9bbcd4f84
Ahsan <80054519+Ahsan3944@users.noreply.github.com>
2026-09-10 00:20:48 +0530

    Fix motion evaluator timing assertion

--- src/test/java/com/ultraop/aurareplay/motion/MotionEvaluatorTest.java
+++ src/test/java/com/ultraop/aurareplay/motion/MotionEvaluatorTest.java
@@ -37,7 +37,7 @@ class MotionEvaluatorTest {
 
         MotionEvaluator.Evaluation result = new MotionEvaluator().evaluate(10, 20, ActorTransform.origin(0, 0, 0, 0, 0), stack);
 
-        assertEquals(4, result.sourcePosition());
+        assertEquals(18, result.sourcePosition());
         assertEquals(true, result.loop());
         assertEquals(true, result.reverse());
     }
