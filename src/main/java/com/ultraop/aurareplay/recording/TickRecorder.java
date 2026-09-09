d60773695ccb9f7a8200069567a8d597aeeadf1a
Ahsan <80054519+Ahsan3944@users.noreply.github.com>
2026-09-09 12:52:16 +0530

    fix: use Paper profile API for captured skin properties

--- src/main/java/com/ultraop/aurareplay/recording/TickRecorder.java
+++ src/main/java/com/ultraop/aurareplay/recording/TickRecorder.java
@@ -8,8 +8,8 @@ import org.bukkit.Bukkit;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.profile.PlayerProfile;
-import org.bukkit.profile.ProfileProperty;
+import com.destroystokyo.paper.profile.PlayerProfile;
+import com.destroystokyo.paper.profile.ProfileProperty;
 
 import java.util.ArrayList;
 import java.util.List;
