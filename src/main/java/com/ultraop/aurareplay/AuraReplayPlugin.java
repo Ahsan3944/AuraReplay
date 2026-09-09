d36398ef1a5190b7e8bd01caf0a2defcb65d4994
Ahsan <80054519+Ahsan3944@users.noreply.github.com>
2026-09-09 16:12:34 +0530

    ui: register Scene Studio editor listener

--- src/main/java/com/ultraop/aurareplay/AuraReplayPlugin.java
+++ src/main/java/com/ultraop/aurareplay/AuraReplayPlugin.java
@@ -4,6 +4,7 @@ import com.comphenix.protocol.ProtocolLibrary;
 import com.comphenix.protocol.ProtocolManager;
 import com.ultraop.aurareplay.command.AuraReplayCommand;
 import com.ultraop.aurareplay.core.AuraEngine;
+import com.ultraop.aurareplay.ui.SceneStudioListener;
 import com.ultraop.aurareplay.ui.StudioCommand;
 import com.ultraop.aurareplay.ui.StudioListener;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -17,6 +18,7 @@ public final class AuraReplayPlugin extends JavaPlugin {
         if(getCommand("aurareplay")!=null){getCommand("aurareplay").setExecutor(command);getCommand("aurareplay").setTabCompleter(command);}
         if(getCommand("arstudio")!=null)getCommand("arstudio").setExecutor(new StudioCommand(engine.studioController()));
         getServer().getPluginManager().registerEvents(new StudioListener(engine.studioController()),this);
+        getServer().getPluginManager().registerEvents(new SceneStudioListener(engine),this);
         getLogger().info("AuraReplay foundation enabled.");
     }
     @Override public void onDisable(){if(engine!=null)engine.shutdown();}
