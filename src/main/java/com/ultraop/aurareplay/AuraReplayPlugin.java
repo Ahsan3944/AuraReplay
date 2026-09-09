e62df6ad81f7f54450aa9ea92368c934a0c10c8e
Ahsan <80054519+Ahsan3944@users.noreply.github.com>
2026-09-09 14:19:19 +0530

    feat: register in-game Studio listener

--- src/main/java/com/ultraop/aurareplay/AuraReplayPlugin.java
+++ src/main/java/com/ultraop/aurareplay/AuraReplayPlugin.java
@@ -4,33 +4,19 @@ import com.comphenix.protocol.ProtocolLibrary;
 import com.comphenix.protocol.ProtocolManager;
 import com.ultraop.aurareplay.command.AuraReplayCommand;
 import com.ultraop.aurareplay.core.AuraEngine;
+import com.ultraop.aurareplay.ui.StudioListener;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class AuraReplayPlugin extends JavaPlugin {
-
     private AuraEngine engine;
-
-    @Override
-    public void onEnable() {
-        ProtocolManager protocolManager = ProtocolLibrary.getProtocolManager();
-        this.engine = new AuraEngine(this, protocolManager);
-        this.engine.start();
-
-        AuraReplayCommand command = new AuraReplayCommand(engine);
-        getCommand("aurareplay").setExecutor(command);
-        getCommand("aurareplay").setTabCompleter(command);
-
+    @Override public void onEnable() {
+        ProtocolManager protocolManager=ProtocolLibrary.getProtocolManager();
+        engine=new AuraEngine(this,protocolManager); engine.start();
+        AuraReplayCommand command=new AuraReplayCommand(engine);
+        if(getCommand("aurareplay")!=null){getCommand("aurareplay").setExecutor(command);getCommand("aurareplay").setTabCompleter(command);}
+        getServer().getPluginManager().registerEvents(new StudioListener(engine.studioController()),this);
         getLogger().info("AuraReplay foundation enabled.");
     }
-
-    @Override
-    public void onDisable() {
-        if (engine != null) {
-            engine.shutdown();
-        }
-    }
-
-    public AuraEngine engine() {
-        return engine;
-    }
+    @Override public void onDisable(){if(engine!=null)engine.shutdown();}
+    public AuraEngine engine(){return engine;}
 }
