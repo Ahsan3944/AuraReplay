64873f6e2b219f50d96d104f90eb47345961b133
Ahsan <80054519+Ahsan3944@users.noreply.github.com>
2026-09-10 18:31:31 +0530

    Correct realtime scheduler expectations

--- src/test/java/com/ultraop/aurareplay/director/DirectorRealtimeRenderSchedulerTest.java
+++ src/test/java/com/ultraop/aurareplay/director/DirectorRealtimeRenderSchedulerTest.java
@@ -8,8 +8,12 @@ class DirectorRealtimeRenderSchedulerTest {
     @Test
     void thirtyFpsEmitsOneOrTwoFramesPerServerTickWithoutDrift() {
         DirectorRealtimeRenderScheduler scheduler = new DirectorRealtimeRenderScheduler(30);
-        int[] expected = {1, 1, 2, 1, 2, 1, 2, 1, 2, 1};
-        for (int value : expected) assertEquals(value, scheduler.advance());
+        int[] expected = {1, 2, 1, 2, 1, 2, 1, 2, 1, 2};
+        for (int value : expected) {
+            int due = scheduler.advance();
+            assertEquals(value, due);
+            scheduler.markEmitted(due);
+        }
         assertEquals(15, scheduler.emittedFrames());
         assertEquals(10, scheduler.elapsedTicks());
     }
@@ -17,14 +21,19 @@ class DirectorRealtimeRenderSchedulerTest {
     @Test
     void sixtyFpsEmitsThreeFramesPerServerTick() {
         DirectorRealtimeRenderScheduler scheduler = new DirectorRealtimeRenderScheduler(60);
-        for (int i = 0; i < 10; i++) assertEquals(3, scheduler.advance());
+        for (int i = 0; i < 10; i++) {
+            int due = scheduler.advance();
+            assertEquals(3, due);
+            scheduler.markEmitted(due);
+        }
         assertEquals(30, scheduler.emittedFrames());
     }
 
     @Test
     void resetStartsTimingAgain() {
         DirectorRealtimeRenderScheduler scheduler = new DirectorRealtimeRenderScheduler(24);
-        scheduler.advance();
+        int due = scheduler.advance();
+        scheduler.markEmitted(due);
         scheduler.reset();
         assertEquals(0, scheduler.emittedFrames());
         assertEquals(0, scheduler.elapsedTicks());
