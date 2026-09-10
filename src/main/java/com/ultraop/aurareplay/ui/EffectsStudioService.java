c468357c9b9acffc3c7ef32b750ae86a374fcbdb
Ahsan <80054519+Ahsan3944@users.noreply.github.com>
2026-09-10 10:39:39 +0530

    Fix Effects Studio List import

--- src/main/java/com/ultraop/aurareplay/ui/EffectsStudioService.java
+++ src/main/java/com/ultraop/aurareplay/ui/EffectsStudioService.java
@@ -13,6 +13,7 @@ import org.bukkit.inventory.meta.ItemMeta;
 
 import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
